dc1bcbe09f32b171dbd4d7e9cf80ec8912babc63
Alex <dudov.al16@gmail.com>
2020-06-11 23:07:59 +0200

    Remove console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Clientside js");
-
 const weatherForm = document.querySelector("form");
 const inputElement = document.querySelector("input");
 const messageOne = document.querySelector('#message-1')
