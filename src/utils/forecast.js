f0bcb074e323b5737ba496c5a937947209dec25e
Alex <dudov.al16@gmail.com>
2020-06-12 16:19:05 +0200

    Changes a message in Forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,7 +13,7 @@ const forecast = (lat, long, callback) => {
       } else if (body.error) {
         callback("Unable to find location", undefined);
       } else {
-        callback(undefined, body.daily.summary+ ' It is currently ' +body.currently.temperature+ ' in ' +body.timezone)
+        callback(undefined, body.daily.summary+ ' It is currently ' +body.currently.temperature + ' with max temperature ' +body.daily.data[0].temperatureHigh + ' and low temperature ' + body.daily.data[0].temperatureLow)
       }
     });
   };
